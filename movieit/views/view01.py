c4043966d1158d96b4e880770bf2fadc63f7b270
jjnwhy <comsa09@naver.com>
2022-09-21 15:59:14 +0900

    조회수 증가방지

--- movieit/views/view01.py
+++ movieit/views/view01.py
@@ -80,8 +80,6 @@ def contentFunc(request):
     print(request.session.session_key)
     data.save()
         
-    
-    
     return render(request, 'content.html', {'data_one':data, 'page':page})
 
 def contentokFunc(request):
